265ca0bf12fb7424f3c2edf246d6af0f110d2822
s_yamashita <s_yamashita@LAPTOP-BVLVOOQQ>
2024-04-12 15:06:39 +0900

    LoginActionを追加しました。

---
+++ src/main/java/authentication/LoginAction.java
@@ -0,0 +1,31 @@
+package authentication;
+
+import bean.Teacher;
+import dao.TeacherDAO;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import tool.Action;
+
+public class LoginAction extends Action {
+	public String execute(
+		HttpServletRequest request, HttpServletResponse response
+	) throws Exception {
+
+		HttpSession session=request.getSession();
+
+		String login=request.getParameter("login");
+		String password=request.getParameter("password");
+
+		TeacherDAO dao=new TeacherDAO();
+		Teacher customer=dao.search(login, password);
+		
+		if (customer!=null) {
+			session.setAttribute("customer", customer);
+			return "login-out.jsp";
+		}
+		
+		return "login-error.jsp";
+	}
+}
+
